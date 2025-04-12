ca98838331c705f9093dc6d55bae3c61f90397b7
jaypathar <jay.pathar@agrostar.in>
2025-04-12 22:38:05 +0530

    setting python path

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 python3 -m venv ../env
 source ../env/bin/activate
 pip install -r requirements.txt
+export PYTHONPATH=$(pwd)
 
 # Add your LambdaTest credentials below
 export LT_USERNAME=""  # ADD LAMBDATEST USERNAME
