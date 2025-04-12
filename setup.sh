d41b995bab61bc3133c4ba1ac87875d44bcac61b
jaypathar <jay.pathar@agrostar.in>
2025-04-12 20:46:30 +0530

    minor change in creds

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@ python3 -m venv ../env
 source ../env/bin/activate
 pip install -r requirements.txt
 
-export LT_USERNAME="jhanvipathar19"
-export LT_ACCESS_KEY="LT_kO094ug1C9tIWGoAQHy6qjJfuUqZcUPholGjZOvMDnWUxnH"
+export LT_USERNAME="patharjay20"
+export LT_ACCESS_KEY="LT_khlHTTfml8bdWEKsEKrimss8wz8kLqNnzdCAx5HrOYR9s2t"
 
 alias runSeq='pytest tests/test_amazon_cart.py '
 alias runParallel='pytest tests/test_amazon_cart.py -s -v -n 2' 
